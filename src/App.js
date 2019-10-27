4828d954136472e5b00409a21d23dd36b3ef08ef
Kurt <deininger.k@gmail.com>
2019-10-27 14:43:02 -0500

    Added page navigation and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <Router>
+      <div>
+        <nav>
+              <Link to="/">Latest</Link>
+              <Link to="/search">Search</Link>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/">
+            <Latest />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Latest() {
+  return <h2>Latest</h2>;
+}
+
+function Search() {
+  return <h2>Search</h2>;
 }
 
 export default App;
